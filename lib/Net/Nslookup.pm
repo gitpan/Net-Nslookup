ea0807c6be0aed6635dff9444bfbf15a46fb2428
Darren Chamberlain <darren@cpan.org>
2009-10-22 11:42:40 +0000

    Import of DARREN/Net-Nslookup-1.19 from CPAN.
    gitpan-cpan-distribution: Net-Nslookup
    gitpan-cpan-version:      1.19
    gitpan-cpan-path:         DARREN/Net-Nslookup-1.19.tar.gz
    gitpan-cpan-author:       DARREN
    gitpan-cpan-maturity:     released

--- lib/Net/Nslookup.pm
+++ lib/Net/Nslookup.pm
@@ -1,8 +1,6 @@
 package Net::Nslookup;
 
 # -------------------------------------------------------------------
-# $Id: Nslookup.pm,v 1.11 2008/04/15 15:20:54 dlc Exp $
-# -------------------------------------------------------------------
 #  Net::Nslookup - Provide nslookup(1)-like capabilities
 #  Copyright (C) 2002 darren chamberlain <darren@cpan.org>
 #
@@ -25,7 +23,7 @@ use strict;
 use vars qw($VERSION $DEBUG $DEBUG_NET_DNS @EXPORT $TIMEOUT $MX_IS_NUMERIC $WIN32);
 use base qw(Exporter);
 
-$VERSION = 1.18;
+$VERSION = 1.19;
 @EXPORT  = qw(nslookup);
 $DEBUG   = 0 unless defined $DEBUG;
 $DEBUG_NET_DNS = 0 unless defined $DEBUG_NET_DNS;
@@ -43,7 +41,7 @@ use Socket qw/ inet_ntoa inet_aton /;
 
 my %_lookups = (
     'a'     => \&_lookup_a,
-    'cname' => \&_lookup_a,
+    'cname' => \&_lookup_cname,
     'mx'    => \&_lookup_mx,
     'ns'    => \&_lookup_ns,
     'ptr'   => \&_lookup_ptr,
@@ -142,6 +140,21 @@ sub _lookup_ns {
     $query = $res->search($term, "NS") || return;
     for $rr ($query->answer) {
         push @answers, nslookup(type => "A", host => $rr->nsdname);
+    }
+
+    return @answers;
+}
+
+sub _lookup_cname {
+    my ($term, $server) = @_;
+    my $res = ns($server);
+    my (@answers, $query, $rr);
+
+    debug("Performing 'CNAME' lookup on `$term'");
+
+    $query = $res->search($term, "CNAME") || return;
+    for $rr ($query->answer) {
+        push @answers, $rr->cname;
     }
 
     return @answers;
